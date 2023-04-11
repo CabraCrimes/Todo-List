b604a0f2c244dacb1efdbce4108b371ac94b862f
CabraCrimes <benalexanderthegreat@gmail.com>
2023-04-11 19:29:12 +0000

    Todo List no CSS

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const [isTodo, setTodo] = useState("")
+const [isArrayTodo, setArrayTodo] = useState([])
+
+
+	return(
+		<div className="container text-center">
+
+		<h1>My List Of Things Todo</h1>
+			<input type="text" 
+			placeholder="Type here"  
+			value={isTodo} 
+			onChange={e=> {setTodo(e.target.value) }}
+			onKeyUp={e=> {
+				if ( e.key === "Enter"){
+				setArrayTodo([...isArrayTodo, isTodo]);
+				setTodo("")}}}
+			
+			/>
+
+			 {/* // When using map we creat a new array. The newisTodo is the new array element. */}
+		<ul>
+			{isArrayTodo.map((newisTodo, index) => {
+				return <li key={index}>{newisTodo}<i className="fa-solid fa-xmark" 
+					onClick={() => setArrayTodo(isArrayTodo.filter((el, newIndex) => {return newIndex != index}))
+				
+			}></i></li>})}
+		</ul>
+
 		</div>
-	);
+	)
 };
 
 export default Home;
